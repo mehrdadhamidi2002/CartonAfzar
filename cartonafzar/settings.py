f90afdf9014a8c4e3e9bb8f8c1ff1c3b5e9df8c5
mehrdadhamidi2002@yahoo.com <mehrdadhamidi2002@yahoo.com>
2024-03-23 14:53:47 +0330

    after whitenoise storage and cache and compress settings

--- cartonafzar/settings.py
+++ cartonafzar/settings.py
@@ -120,9 +120,20 @@ STATIC_URL = '/static/'
   #  STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'tss/static'),]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 print (STATIC_ROOT)
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
